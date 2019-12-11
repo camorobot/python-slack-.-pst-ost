b2649b651be5e6397b2ad7f43ac4c229b5e0c5e7
Kevin <41551968+camorobot@users.noreply.github.com>
2019-12-11 16:23:16 +0100

    Add slack api token

---
+++ main.py
@@ -0,0 +1,23 @@
+########################
+# TO DO
+########################
+# 1 find ost and pst files on the computer
+# 2 import slack api
+# 3 make a .zip file off all pst and ost files
+# 4 upload .zip file with the slack api
+########################
+# INFO
+########################
+# command promt: dir /s *.pst /AND/ dir /s *.ost
+########################
+
+import os
+import subprocess
+from slacker import Slacker
+
+TOKEN = open('TOKEN.txt', 'r')
+
+slack = Slacker(TOKEN)
+
+message = "test"
+slack.chat.post_message('#random',message);
